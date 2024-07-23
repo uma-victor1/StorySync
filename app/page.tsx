a9e489858be48dead8bccba858d3ae82629438d7
Victor Uma <umavictor11@gmail.com>
2024-07-23 23:27:12 +0100

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import FindKeyword from "@/components/FindKeyword/FindKeyword";
+import JoinCommunity from "@/components/JoinCommunity/JoinCommunity";
+import Landing from "@/components/Landing";
+import OurProcess from "@/components/OurProcess/OurProcess";
+import OurUsers from "@/components/OurUsers/OurUsers";
+import Readers from "@/components/Readers/Readers";
+import Services from "@/components/Services";
+import Testimonials from "@/components/Testimonials/Testimonials";
+import { Contact } from "lucide-react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col gap-8 pb-0 pt-6"></main>
+    <main className="flex min-h-screen w-full flex-col gap-8 pb-0 pt-6">
+      <Landing />
+      <Services />
+      <OurUsers />
+      <OurProcess />
+      <JoinCommunity />
+      <FindKeyword />
+      <Testimonials />
+      <Readers />
+      <Contact />
+    </main>
   );
 }
