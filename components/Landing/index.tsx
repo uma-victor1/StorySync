ea03877c90f8d75f669c0530fe19427cc2caa04c
Victor Uma <umavictor11@gmail.com>
2024-07-25 22:36:48 +0100

    style button

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -26,7 +26,7 @@ const Landing = () => {
   const { transformX: tx, transformY: ty } = useCursorParallax(3000);
   return (
     <header className="hero-section site-section overflow-x-clip">
-      <div className="dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative z-10 flex  h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+      <div className="relative z-10 flex h-[50rem] w-full  items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2]">
         {/* Radial gradient for the container to give a faded look */}
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
         <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
@@ -47,6 +47,7 @@ const Landing = () => {
                 color="ghost"
                 hasIcon={true}
                 isFlexInv={true}
+                className="bg-white bg-opacity-80"
                 icon={
                   <ArrowRight
                     variant="TwoTone"
